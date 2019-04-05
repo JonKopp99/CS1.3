9ab2b97d66967cd1f9ec6ad8d661b84fab69a267
JonKopp99 <jonkopp99@gmail.com>
2019-04-05 15:18:28 -0700

    Added factorial

---
+++ Decimal-Binary/Decimal-Binary.py
@@ -0,0 +1,86 @@
+#!python
+
+import string
+# Hint: Use these string constants to encode/decode hexadecimal digits and more
+# string.digits is '0123456789'
+# string.hexdigits is '0123456789abcdefABCDEF'
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+# string.printable is digits + ascii_letters + punctuation + whitespace
+
+
+def decode(digits, base):
+    """Decode given digits in given base to number in base 10.
+    digits: str -- string representation of number (in given base)
+    base: int -- base of given number
+    return: int -- integer representation of number (in base 10)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # TODO: Decode digits from binary (base 2)
+    # ...
+    hex = string.printable#string.digits + string.ascii_uppercase + "+/"
+    digits = digits[::-1].lower()
+    hexTuple = enumerate(digits)
+    final_num = 0
+    for power, value in hexTuple:
+        final_num += hex.index(value)*(base ** power)
+    return final_num
+
+
+def encode(number, base):
+    """Encode given number in base 10 to digits in given base.
+    number: int -- integer representation of number (in base 10)
+    base: int -- base to convert to
+    return: str -- string representation of number (in given base)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # Handle unsigned numbers only for now
+    assert number >= 0, 'number is negative: {}'.format(number)
+    final_hex = []
+    hex = string.digits + string.ascii_uppercase + "+/"
+    if base == 64:
+        print("Base64")
+        hex = string.ascii_uppercase + string.lowercase + string.digits + "+/"
+    while number != 0:
+        r = number%base
+        final_hex.append(hex[r])
+        number = number//base
+    final_hex = final_hex[::-1] #reverses the array
+    final_number_string = "".join(final_hex)
+    return final_number_string
+
+
+def convert(digits, base1, base2):
+    """Convert given digits in base1 to digits in base2.
+    digits: str -- string representation of number (in base1)
+    base1: int -- base of given number
+    base2: int -- base to convert to
+    return: str -- string representation of number (in base2)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
+    assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
+    return encode(decode(digits,base1),base2)
+
+
+def main():
+    """Read command-line arguments and convert given digits between bases."""
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 3:
+        digits = args[0]
+        base1 = int(args[1])
+        base2 = int(args[2])
+        # Convert given digits between bases
+        result = convert(digits, base1, base2)
+        print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
+    else:
+        print('Usage: {} digits base1 base2'.format(sys.argv[0]))
+        print('Converts digits from base1 to base2')
+
+
+if __name__ == '__main__':
+    print(encode(42, 16))
+    print(decode("101010", 10))
+    print(convert('1100100001000000', 2, 16))
+    #main()
