f878ef24ddf79156700cad2d9ae29d051b73eb1c
JonKopp99 <jonkopp99@gmail.com>
2019-04-21 16:47:04 -0700

    Added queue module

---
+++ linkedList/queue.py
@@ -0,0 +1,105 @@
+from linkedlist import LinkedList
+
+
+# Implement LinkedQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class LinkedQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        return self.list.is_empty()
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return self.list.length()
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(???)  Why? [TODO]"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.is_empty() == True:
+            return None
+
+        return self.list.head.data
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(???)  Why? [TODO]"""
+        if self.list.head == None:
+            raise ValueError('Empty')
+        item = self.list.head.data
+        self.list.delete(self.list.head.data)
+        return item
+
+
+# Implement ArrayQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class ArrayQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        if(len(self.list) >= 1):
+            return False
+        return True
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return len(self.list)
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(???)  Why? [TODO]"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if(length() > 0):
+            return self.list[0]
+        return None
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(???)  Why? [TODO]"""
+        if self.length() < 0:
+            raise ValueError('List is empty')
+        item = self.list[0]
+        self.list.pop(0)
+
+        return item
+
+
+# Implement LinkedQueue and ArrayQueue above, then change the assignment below
+# to use each of your Queue implementations to verify they each pass all tests
+Queue = LinkedQueue
+# Queue = ArrayQueue
