69fb1876491606af4325225521304ef925682bb4
JonKopp99 <jonkopp99@gmail.com>
2019-05-11 15:04:38 -0700

    Fixed set class

--- Set/set.py
+++ Set/set.py
@@ -25,14 +25,15 @@ class set(object):
         if(self.contains(item)):
             return False
         self.list.append(item)
+        self.size += 1
         return True
 
     def remove(self, item):
-            if self.contains(item):
-                self.list.remove(item)
-                self.size -= 1
-                return True
-            return False
+        if self.contains(item):
+            self.list.remove(item)
+            self.size -= 1
+            return True
+        return False
 
     def intersection(self, otherSet):
         newSet = set()
@@ -83,37 +84,37 @@ class set(object):
                 return False
         return True
 
-if __name__ == '__main__':
-    #Basic functions test
-    testAr = [1,3,5,2,4]
-    theSet = set(testAr)
-    print(theSet.length())
-    assert theSet.length() == 5
-    theSet.remove(3)
-    print("Does set contain 3?: " , theSet.contains(3))
-    print(theSet.length())
-    assert theSet.length() == 4
-
-    #Intersection test
-    testAr2 = [1,3,5]
-    theSet2 = set(testAr2)
-    newIntSet = theSet.intersection(theSet2)
-    print("Intersection of lists: ", newIntSet.list)
-    assert newIntSet.list == [1, 5]
-
-    #Union test
-    newUnionSet = theSet.union(newIntSet)
-    print("Union of the lists: ", newUnionSet.list)
-    assert newUnionSet.list == [1,5,2,4]
-
-    #Difference test
-    set1 = set([1,2,3,4,5])
-    set2 = set([6,7,8,3,10])
-    newDiffSet = set1.difference(set2)
-    print("Diffrence between the two sets: ", newDiffSet.list)
-
-    #Subset test_set_twice_and_get
-    set3 = set([1,2,3])
-    set4 = set([1,4,3,2,5,6])
-    print("Is subset: ", set3.is_subset(set4))
-    print("Is subset: ", set4.is_subset(set3))
+# if __name__ == '__main__':
+#     #Basic functions test
+#     testAr = [1,3,5,2,4]
+#     theSet = set(testAr)
+#     print(theSet.length())
+#     assert theSet.length() == 5
+#     theSet.remove(3)
+#     print("Does set contain 3?: " , theSet.contains(3))
+#     print(theSet.length())
+#     assert theSet.length() == 4
+#
+#     #Intersection test
+#     testAr2 = [1,3,5]
+#     theSet2 = set(testAr2)
+#     newIntSet = theSet.intersection(theSet2)
+#     print("Intersection of lists: ", newIntSet.list)
+#     assert newIntSet.list == [1, 5]
+#
+#     #Union test
+#     newUnionSet = theSet.union(newIntSet)
+#     print("Union of the lists: ", newUnionSet.list)
+#     assert newUnionSet.list == [1,5,2,4]
+#
+#     #Difference test
+#     set1 = set([1,2,3,4,5])
+#     set2 = set([6,7,8,3,10])
+#     newDiffSet = set1.difference(set2)
+#     print("Diffrence between the two sets: ", newDiffSet.list)
+#
+#     #Subset test_set_twice_and_get
+#     set3 = set([1,2,3])
+#     set4 = set([1,4,3,2,5,6])
+#     print("Is subset: ", set3.is_subset(set4))
+#     print("Is subset: ", set4.is_subset(set3))
---
+++ Set/setTest.py
@@ -0,0 +1,71 @@
+from set import set
+import unittest
+
+class setTest(unittest.TestCase):
+
+    def testAddAndSize(self):
+        print("Testing add and size!")
+        testAr = [1,3,5,2,4]
+        theSet = set(testAr)
+        assert theSet.size == 5
+        theSet.add(7)
+        assert theSet.size == 6
+        theSet.add(2)
+        assert theSet.size == 6
+        theSet.add(8)
+        assert theSet.size == 7
+
+    def testRemoveAndContains(self):
+        print("Testing remove and contains")
+        testAr = ["a", "b", "c", "d", "e", "f", "g"]
+        theSet = set(testAr)
+        assert theSet.size == 7
+        assert theSet.contains("a") == True
+        assert theSet.remove("a") == True
+        assert theSet.contains("a") == False
+        assert theSet.remove("b") == True
+        assert theSet.contains("b") == False
+        assert theSet.remove("c") == True
+        assert theSet.contains("c") == False
+        assert theSet.remove("d") == True
+        assert theSet.contains("d") == False
+
+    def testIntersection(self):
+        print("Testing interseciton")
+        theSet1 = set([1,3,5,2,4])
+        theSet2 = set([1,3,5])
+        assert theSet1.intersection(theSet2).list == [1,3,5]
+        theSet1 = set(["a", "b", "c", "d", "e", "f", "g"])
+        theSet2 = set(["a", "b", "c", "d", "e", "f", "g"])
+        assert theSet1.intersection(theSet2).list == ["a", "b", "c", "d", "e", "f", "g"]
+        assert theSet2.remove("b") == True
+        assert theSet1.intersection(theSet2).list == ["a", "c", "d", "e", "f", "g"]
+
+    def testUnion(self):
+        print("Testing union!")
+        theSet1 = set([2,4])
+        theSet2 = set([1,3,5])
+        assert theSet1.union(theSet2).list == [1,3,5,2,4]
+        theSet1 = set(["a", "b", "c"])
+        theSet2 = set(["d", "e", "f", "g"])
+        assert theSet1.union(theSet2).list == ["d", "e", "f", "g", "a", "b", "c"]
+        assert theSet1.remove("c") == True
+        assert theSet1.union(theSet2).list == ["d", "e", "f", "g", "a", "b"]
+
+    def testIsSubset(self):
+        print("Testing is_subset!")
+        theSet1 = set([1,3,5,2,4])
+        theSet2 = set([1,3,5])
+        assert theSet2.is_subset(theSet1) == True
+        assert theSet1.is_subset(theSet2) == False
+        theSet1 = set(["a", "b", "c", "d", "e", "f", "g"])
+        theSet2 = set(["a", "b", "c", "d", "e"])
+        assert theSet2.is_subset(theSet1) == True
+        assert theSet1.is_subset(theSet2) == False
+        theSet1 = set(["jon", "is", "the", "coolest"])
+        theSet2 = set(["jon", "is", "coolest"])
+        assert theSet2.is_subset(theSet1) == True
+        assert theSet1.is_subset(theSet2) == False
+
+if __name__ == '__main__':
+    unittest.main()
