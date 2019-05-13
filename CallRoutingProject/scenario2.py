c6143e858c592726f1ee6c8cc2555d385a10c624
JonKopp99 <jonkopp99@gmail.com>
2019-05-12 21:06:37 -0700

    Scenario2 implemented with dictionary

---
+++ CallRoutingProject/scenario2.py
@@ -0,0 +1,52 @@
+"""
+You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a
+single phone number. How quickly can you find the cost of calling this number?
+1)Seperate 1000 phone#'s into array'
+2)Split the routes into a dictionary for constant time Searching
+    -When splitting seperate by comma
+    -Check if newValue < oldVal only update if its less
+3)Loop through len([phone#s])substringing last # off every iteraiton
+4)If []results ->helper function to find lowest route cost
+"""
+def splitIntoArray(path):
+    with open(path) as file:
+        lines = file.read().splitlines()
+    return lines
+#O(n)where n is the length of the words
+def splitIntoDict(path):
+    with open(path) as file:
+        lines = file.read().splitlines()
+    theDict = {}
+    for line in lines:
+        mylist = line.split(',')
+        if(mylist[0] in theDict.keys()):
+            oldVal = theDict[mylist[0]]
+            if(oldVal > mylist[1]):
+                theDict[mylist[0]] = mylist[1]
+        else:
+             theDict[mylist[0]] = mylist[1]
+    return theDict
+
+#O(1) Best case if the phone number is a route
+#O(n) N being lenth of phonenumber string if there are not matches for routes
+def findMatches(pn, theDict):
+    for phoneNum in pn:
+        ctr = len(phoneNum) - 1
+        loop = True
+        while(loop):
+            s = phoneNum[0:ctr]
+            #print(s)
+            if(s in theDict.keys()):
+                loop = False
+                print(theDict[s])
+            ctr -= 1
+            if(ctr <= 1):
+                loop = False
+
+
+
+if __name__ == '__main__':
+    theArray = splitIntoArray('pn100.txt')
+    theDict = splitIntoDict('rc100.txt')
+    #print(theDict)
+    print(findMatches(theArray, theDict))
