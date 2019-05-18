79e3df53286fcd66af1c2bb93322297f368a1b65
Jonathan Kopp <jonkopp99@gmail.com>
2019-05-17 17:28:05 -0700

    Update trie.py

--- CallRoutingProject/trie.py
+++ CallRoutingProject/trie.py
@@ -1,8 +1,8 @@
 from numbers_prefix_cost import *
+import timeit
 import resource
 import platform
-import timeit
-#Worked on with Ramon Geronimo
+#worked on with Ramon Geronimo
 CHILDREN_SIZE = 10
 
 class Node:
@@ -16,12 +16,11 @@ class Trie:
         # The root node is an empty node we are using +
         self.root = Node('+')
 
-    def insert(self, numbers, cost):
-        path = []
+    def insert(self, prefix, cost):
         # Start at the root node
         current = self.root
         # Numbers is a string of numbers and number is a char number
-        for number in numbers:
+        for number in prefix:
             # We use ascii representation to get the index and transfer within the range
             # [0, CHILDREN_SIZE - 1]
             ascii_index = ord(number)-ord('0')
@@ -29,20 +28,19 @@ class Trie:
             if current.children[ascii_index]:
                 # Keep going
                 current = current.children[ascii_index]
-                path.append(current.data)
             else:
                 # Insert a new node
                 node = Node(number)
                 current.children[ascii_index] = node
                 current = node
-                path.append(current.data)
+                
 
         # leaf nodes represent the end of the words in the tree
-        current.data = (''.join(path), cost)
+        current.data = (prefix, cost)
         current.leaf = True
-        # print("Current Data", current.data)
+        
 
-
+        
 
     def search(self, numbers):
         # if the tree is empty
@@ -61,95 +59,49 @@ class Trie:
                 if isinstance(current.data, tuple) and current.data[0] in numbers:
                     # Not leaf but contain prefixd
                     # print(current.data)
-                    print("Data: ", current.data)
+                    print("Phone: {} prefix {} cost {}".format(numbers, current.data[0], current.data[1]))
                     return current.data
                 else:
                     # Number is not present
                     return False
         # if we consider all numbers and the node is a leaf we found the number
         if current.leaf and current.data[0] in numbers:
-            print("Current Data:", current.data)
+            print("Phone: {} prefix {} cost {} Leaf Node.".format(numbers, current.data[0], current.data[1]))
             return True
         # number is not present in the tree
         return False
 
 
     def insert_file(self, route_costs):
-        index = 0
-        while index < len(route_costs):
-            # Start at the root node
-            current = self.root
-            # Numbers is a string of numbers and number is a char number
-            for number in route_costs[index][0]:
-                # We use ascii representation to get the index and transfer within the range
-                # [0, CHILDREN_SIZE - 1]
-                ascii_index = ord(number)-ord('0')
-                # Check if theres a child with given number
-                if current.children[ascii_index]:
-                    # Keep going
-                    current = current.children[ascii_index]
-                else:
-                    # Insert a new node
-                    node = Node(number)
-                    current.children[ascii_index] = node
-                    current = node
-
-            # leaf nodes represent the end of the words in the tree
-            current.data = (route_costs[index][0], route_costs[index][1])
-            current.leaf = True
-            # print("Current Data", current.data)
-            index += 1
-
-
+        for data in route_costs:
+            prefix = data[0]
+            cost = data[1]
+            self.insert(prefix, cost)
+        
+        
 
     def search_file(self, numbers_file):
-        # if the tree is empty
-        if not self.root.children:
-            return False
-        index = 0
-        while index < len(numbers_file):
-            # start with the root node
-            current = self.root
-            # consider all the root node
-            for number in numbers_file[index]:
-                # print("Number: ", numbers_file)
-                ascii_index = ord(number)-ord('0')
-                # check if number is present in the tree
-                if current.children[ascii_index]:
-                    # Keep going
-                    current = current.children[ascii_index]
-                else:
+        for number in numbers_file:
+            self.search(number)
+       
 
-                    if isinstance(current.data, tuple) and current.data[0] in numbers_file[index]:
-                        # Not leaf but contain prefixd
-                        # print(current.data)
-                        # print(current.data)
-                        print("Current Data: ", current.data)
-                        return current.data
-                    else:
-                        # Number is not present
-                        continue
-            index += 1
-            # if we consider all numbers and the node is a leaf we found the number
-            if current.leaf and current.data[0] in numbers_file:
-                print("Current Data:", current.data)
-                return True
-            # number is not present in the tree
-            return False
+            
 
 
-
-
-
+    
 if __name__ == "__main__":
-
+    
     trie = Trie()
-    trie.insert_file(prefix_cost_dict('rc100.txt'))
+    # tup = ('1234', '0.05')
+    # trie.insert(tup[0], tup[1])
+    # trie.search(tup[0])
+    # trie.insert_file(prefix_cost_dict('route-costs-35000.txt'))
+    trie.insert_file(prefix_cost_dict('route-costs-100.txt'))
     # trie.insert('1213', '0.05')
     # print(trie.search('12138881907'))
-    # print(trie.search('1888417'))
-    # print(trie.search_file(number_list('phone-numbers-1000.txt')))
-    print(timeit.timeit("trie.search_file(number_list('pn100.txt'))", globals=globals(), number=1))
+    # print(trie.search('34781380'))
+    # print(trie.search_file(number_list('phone-numbers-10000.txt')))
+    print(timeit.timeit("trie.search_file(number_list('route-costs-35000.txt'))", globals=globals(), number=1))
     usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     usage=round(usage/float(1<<20),2)
 
